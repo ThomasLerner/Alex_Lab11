c07062ba4817a6fd4b6c27305da1b4258ab695c7
Thomas Lerner <tsl26@gmail.com>
2016-05-10 21:41:05 -0700

    my attempt

--- src/SocSecProcessor.java
+++ src/SocSecProcessor.java
@@ -1,27 +1,46 @@
 import java.util.Scanner;
 
 public class SocSecProcessor {
-	
+
 	public static void main(String[] args) {
 		String name, socSecNumber;
 		char nextChar;
 		Scanner keyboard = new Scanner(System.in);
-		
+		System.out.print("Name? ");
+		name = keyboard.nextLine();
+		System.out.print("SSN? ");
+		socSecNumber = keyboard.nextLine();
+
 		do {
 			try {
-				System.out.print("Name? ");
-				name = keyboard.nextLine();
-				System.out.print("SSN? ");
-				socSecNumber = keyboard.nextLine();
+				if(isValid(socSecNumber)){
+					System.out.print(name + " " + socSecNumber);
+				}
 			}
-			catch (new SocSecException()){
-				
-				
+			catch (SocSecException e){
+
+
 			}
-			
+
 			System.out.print("Continue? ");
 			nextChar = keyboard.next().charAt(0);
 		} while (nextChar == 'y' || nextChar == 'Y');
-		
+
+	}
+
+	public static boolean isValid(String ssn) throws SocSecException {
+		if(ssn.length() != 11) {
+			throw new SocSecException("Incorrect length");
+		}
+		for(int x = 0; x < ssn.length(); x++) {
+			if(x == 3 || x == 6) {
+				if(ssn.charAt(x) != '-') {
+					throw new SocSecException("Incorrect hyphen placement");
+				}
+			}
+			else if(ssn.charAt(x) > 57 || ssn.charAt(x) < 48) {
+				throw new SocSecException("Non-numeric character");
+			}
+		}
 	}
 }
