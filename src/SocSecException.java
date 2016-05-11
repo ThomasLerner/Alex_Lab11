7ebf7aed26b3fea456173688424605795f297b1f
Thomas Lerner <tsl26@gmail.com>
2016-05-10 21:10:50 -0700

    fix
    oops

--- src/SocSecException.java
+++ src/SocSecException.java
@@ -3,7 +3,7 @@ public class SocSecException extends Exception {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -2484142775952443501L;
 	
 	public SocSecException(String error) {
 		super("Invalid social security number");
